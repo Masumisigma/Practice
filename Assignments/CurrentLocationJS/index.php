82ccdea88043ec71e43c2b17423be0c434f861ae
Masumisigma <72392434+Masumisigma@users.noreply.github.com>
2020-10-13 22:42:51 +0530

    Create index.php

---
+++ Assignments/CurrentLocationJS/index.php
@@ -0,0 +1,12 @@
+<html xmlns="http://www.w3.org/1999/html">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+<div>
+    You Are Opening My Document From
+<span id="city"></span> , <span id="postal"></span>
+    <script src="script.js"></script>
+</body>
+</html>
