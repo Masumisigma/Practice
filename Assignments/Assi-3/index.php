e5ae58b3a2125a2aa850e2f52f5d29ef496a78de
Masumisigma <72392434+Masumisigma@users.noreply.github.com>
2020-10-20 22:31:24 +0530

    Assi-3 HTML/CSS

---
+++ Assignments/Assi-3/index.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: masumiupadhyay
+ * Date: 14/10/20
+ * Time: 10:15 AM
+ */
+?>
+<!
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Calculator</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<div class="container">
+    <div class="row"><input type="number" placeholder="Calculator"></div>
+    <div class="row">
+        <div class="column"><button>AC</button></div>
+        <div class="column"><button>+/-</button></div>
+        <div class="column"><button>&#8730;</button></div>
+        <div class="column"><button>/</button></div>
+    </div>
+    <div class="row">
+        <div class="column"><button>7</button></div>
+        <div class="column"><button>8</button></div>
+        <div class="column"><button>9</button></div>
+        <div class="column"><button> &#215;</button></div>
+    </div>
+    <div class="row">
+        <div class="column"><button>4</button></div>
+        <div class="column"><button>5</button></div>
+        <div class="column"><button>6</button></div>
+        <div class="column"><button>-</button></div>
+    </div>
+    <div class="row">
+        <div class="column"><button>1</button></div>
+        <div class="column"><button>2</button></div>
+        <div class="column"><button>3</button></div>
+        <div class="column"><button>+</button></div>
+    </div>
+    <div class="row">
+        <div class="column"><button>0</button></div>
+        <div class="column"><button>.</button></div>
+        <div class="column"><button>%</button></div>
+        <div class="column"><button style="background-color: deepskyblue; color: black">=</button></div>
+    </div>
+</div>
+</body>
+</html>
